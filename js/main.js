10f991f0c406bb916840acff19e32b5acfd26e7a
Shiham123 <s36651786@gmail.com>
2024-06-11 12:08:48 +0600

    second slider setup complete

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// const textEl = document.querySelector('.text p');
-// textEl.innerHTML = textEl.innerText
-//   .split('')
-//   .map(
-//     (char, i) => `<span style="transform:rotate(${i * 8.3}deg)">${char}</span>`
-//   )
-//   .join('');
